facd71a0e8f9f53d07c6f8076600cfb66bcfc41c
Lina Reina <linareinap3@gmail.com>
2026-05-16 04:04:39 +0000

    Light: Red, Yellow and green added

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,29 +1,26 @@
-import React from "react";
+import React, { useState } from 'react';
 import '../../styles/home.css';
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+const COLORS = ["red", "yellow", "green"];
 
-//create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-            
+  const [color, setColor] = useState("red");
+  const [showPurple, setShowPurple] = useState(false);
 
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+  const allColors = showPurple ? [...COLORS, "purple"] : COLORS;
+
+  const cycleColor = () => {
+    const i = allColors.indexOf(color);
+    setColor(allColors[(i + 1) % allColors.length]);
+  };
+
+  const togglePurple = () => {
+    setShowPurple(prev => {
+      if (prev && color === "purple") setColor("red");
+      return !prev;
+    });
+  };
+
 };
 
 export default Home;
